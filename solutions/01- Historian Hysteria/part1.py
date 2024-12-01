2c5134bc0113b291350b2e4fb3b12cb6ffe344da
Seb Zanardo <sebzanardo@optusnet.com.au>
2024-12-01 16:55:47 +1100

    Optimised solutions for Day 1

--- solutions/01- Historian Hysteria/part1.py
+++ solutions/01- Historian Hysteria/part1.py
@@ -1,21 +1,30 @@
 import sys
+from heapq import heapify, heappop
 
 
 # Try to read a path to an input file from command line arguments
 path = sys.argv[1] if len(sys.argv) > 1 else "input.in"
 
+# Initialise an array for left and right lists
+left = []
+right = []
+
+# Parse input file appending integers to left and right lists
 with open(path, "r") as f:
-    left = []
-    right = []
     for line in f.readlines():
         l, r = map(int, line.split())
         left.append(l)
         right.append(r)
-    left.sort()
-    right.sort()
 
-    total_difference = 0
-    for i in range(len(left)):
-        total_difference += abs(left.pop() - right.pop())
+# Transform left and right lists into min heaps
+# heapq.heapify is an O(n) time, O(1) space operation
+# heapify is faster than an iterative heappush which is 0(n*log(n)) time
+heapify(left)
+heapify(right)
+
+# Pop off the smallest value on each heap and calculate difference
+total_difference = 0
+for _ in range(len(left)):
+    total_difference += abs(heappop(left) - heappop(right))
 
 print(f"Solution:\n{total_difference}")
--- solutions/01- Historian Hysteria/part2.py
+++ solutions/01- Historian Hysteria/part2.py
@@ -1,19 +1,24 @@
 import sys
+from collections import Counter
 
 
 # Try to read a path to an input file from command line arguments
 path = sys.argv[1] if len(sys.argv) > 1 else "input.in"
 
+# Initialise a counting hashmap for left and right lists
+left = Counter()
+right = Counter()
+
+# Parse input file adding integers to left and right hashmaps
 with open(path, "r") as f:
-    left = []
-    right = []
     for line in f.readlines():
         l, r = map(int, line.split())
-        left.append(l)
-        right.append(r)
+        left[l] += 1
+        right[r] += 1
 
-    total = 0
-    for num in left:
-        total += num * right.count(num)
+# Calculate similarity score
+similarity_score = 0
+for num, occurences in left.items():
+    similarity_score += num * right[num] * occurences
 
-print(f"Solution:\n{total}")
+print(f"Solution:\n{similarity_score}")
