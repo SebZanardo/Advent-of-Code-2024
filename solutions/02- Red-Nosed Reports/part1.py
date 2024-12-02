074c1afa9bf311e91e277d034b978fe5a364829f
Seb Zanardo <sebzanardo@optusnet.com.au>
2024-12-03 07:41:27 +1100

    Optimised solutions for Day 2

--- solutions/02- Red-Nosed Reports/part1.py
+++ solutions/02- Red-Nosed Reports/part1.py
@@ -4,40 +4,32 @@ import sys
 # Try to read a path to an input file from command line arguments
 path = sys.argv[1] if len(sys.argv) > 1 else "input.in"
 
+# Parse input into 2D array of integers
 reports = []
 with open(path, "r") as f:
     for line in f.readlines():
         reports.append(list(map(int, line.split())))
 
+# Walk through each report with two pointers and check direction and difference
+# Two pointers move along pointing to consecutive elements in the report
 safe = 0
 for report in reports:
-    unsafe = False
+    # Find first pair direction to determine report direction
+    # direction = 0: decreasing, 1: increasing
+    report_direction = (report[1] - report[0]) > 0
 
-    last_level = report[0]
-    for i in range(1, len(report)):
-        level = report[i]
-        difference = abs(last_level - level)
-        if difference > 3 or difference < 1:
-            unsafe = True
+    for i in range(len(report) - 1):
+        # Check difference between last two levels is between 1 and 3
+        difference = abs(report[i] - report[i + 1])
+        if (difference < 1 or difference > 3):
             break
-        last_level = level
 
-    if unsafe:
-        continue
-
-    direction = (report[0] - report[1]) > 0
-
-    last_level = report[0]
-    for i in range(1, len(report)):
-        level = report[i]
-        if (last_level - level > 0) != direction:
-            unsafe = True
+        # Check direction matches the direction of the first two levels
+        direction = (report[i + 1] - report[i]) > 0
+        if direction != report_direction:
             break
-        last_level = level
-
-    if unsafe:
-        continue
-
-    safe += 1
+    else:
+        # If the loop wasn't broken then the report is safe
+        safe += 1
 
 print(f"Solution:\n{safe}")
--- solutions/02- Red-Nosed Reports/part2.py
+++ solutions/02- Red-Nosed Reports/part2.py
@@ -1,50 +1,56 @@
 import sys
-from copy import deepcopy
+
+
+def is_super_safe(report: list[int], removed_already: bool = False) -> bool:
+    # Find first pair direction to determine report direction
+    # direction = 0: decreasing, 1: increasing
+    report_direction = (report[1] - report[0]) > 0
+
+    for i in range(len(report) - 1):
+        # Check difference between last two levels is between 1 and 3
+        difference = abs(report[i] - report[i + 1])
+        if (difference < 1 or difference > 3):
+            if removed_already:
+                return False
+            else:
+                # Check if report is safe again with level at pointers removed
+                if is_super_safe(report[:i] + report[i + 1:], True):
+                    return True
+                return is_super_safe(report[:i + 1] + report[i + 2:], True)
+
+        # Check direction matches the direction of the first two levels
+        direction = (report[i + 1] - report[i]) > 0
+        if direction != report_direction:
+            if removed_already:
+                return False
+            else:
+                # Check if report is safe again with level at pointers removed
+                # Edge case:
+                #    if direction is unsafe on second element remove first too
+                if i == 1 and is_super_safe(report[:i - 1] + report[i:], True):
+                    return True
+                if is_super_safe(report[:i] + report[i + 1:], True):
+                    return True
+                return is_super_safe(report[:i + 1] + report[i + 2:], True)
+
+    return True
 
 
 # Try to read a path to an input file from command line arguments
 path = sys.argv[1] if len(sys.argv) > 1 else "input.in"
 
+# Parse input into 2D array of integers
 reports = []
 with open(path, "r") as f:
     for line in f.readlines():
         reports.append(list(map(int, line.split())))
 
-
-def test_if_safe(report) -> bool:
-    last_level = report[0]
-    for i in range(1, len(report)):
-        level = report[i]
-        difference = abs(last_level - level)
-        if difference > 3 or difference < 1:
-            return False
-        last_level = level
-
-    direction = (report[0] - report[1]) > 0
-
-    last_level = report[0]
-    for i in range(1, len(report)):
-        level = report[i]
-        if (last_level - level > 0) != direction:
-            return False
-        last_level = level
-
-    return True
-
-
+# Walk through each report with two pointers and check direction and difference
+# Two pointers move along pointing to consecutive elements in the report
+# If found an unsafe report try remove possible unsafe elements and test again
 safe = 0
 for report in reports:
-    # Safe already
-    if test_if_safe(report):
+    if is_super_safe(report):
         safe += 1
-        continue
-
-    # Need to remove one
-    for i in range(len(report)):
-        report_mod = deepcopy(report)
-        report_mod.pop(i)
-        if test_if_safe(report_mod):
-            safe += 1
-            break
 
 print(f"Solution:\n{safe}")
