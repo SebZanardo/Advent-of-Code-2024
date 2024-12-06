09ed5a8871a613cc959e9582255ad955ea065bd3
Seb Zanardo <sebzanardo@optusnet.com.au>
2024-12-06 11:52:46 +1100

    Optimised solutions for Day 5

--- solutions/05- Print Queue/part1.py
+++ solutions/05- Print Queue/part1.py
@@ -1,43 +1,46 @@
 import sys
+from collections import defaultdict
 
 
-def valid(update) -> bool:
-    for i, n in enumerate(update):
-        if n not in order:
+def is_ordered(update: list[int], ordering_rules: dict[int, list]) -> bool:
+    for i, num in enumerate(update):
+        # If there are no ordering rules for this number skip!
+        if num not in ordering_rules:
             continue
+
+        # Otherwise iterate over every number before in sequence
+        # Return false if a previous page in sequence must be before current
         for j in range(i):
-            not_before = update[j]
-            if not_before in order[n]:
+            previous_num = update[j]
+            if previous_num in ordering_rules[num]:
                 return False
+
     return True
 
 
 # Try to read a path to an input file from command line arguments
 path = sys.argv[1] if len(sys.argv) > 1 else "input.in"
 
-order: dict[int, list[int]] = {}
+# Parse input file into a dictionary for rules and a 2D array for page updates
+ordering_rules = defaultdict(list)
 updates = []
 with open(path, "r") as f:
-    line = f.readline().strip()
+    rules, page_updates = f.read().split("\n\n")
 
-    while line:
-        x, y = map(int, line.split('|'))
-        if x in order:
-            order[x].append(y)
-        else:
-            order[x] = [y]
-        line = f.readline().strip()
+    for rule in rules.split('\n'):
+        x, y = map(int, rule.split('|'))
+        ordering_rules[x].append(y)
 
-    line = f.readline().strip()
-    while line:
-        updates.append(list(map(int, line.strip().split(','))))
-        line = f.readline().strip()
+    for page_update in page_updates.strip().split('\n'):
+        updates.append(list(map(int, page_update.split(','))))
 
+# If update is sorted then sum middle term
 middle_total = 0
 for update in updates:
-    if valid(update):
-        middle = len(update) // 2
-        middle_total += update[middle]
+    if not is_ordered(update, ordering_rules):
+        continue
 
+    middle_index = len(update) // 2
+    middle_total += update[middle_index]
 
 print(f"Solution:\n{middle_total}")
--- solutions/05- Print Queue/part2.py
+++ solutions/05- Print Queue/part2.py
@@ -1,66 +1,75 @@
 import sys
-# I may have quickly tried to random.shuffle() until valid :))
-# import random
-
-print("Sorry this solution is not optimal yet and may take a while to compute")
+from collections import defaultdict
 
 
-def valid(update) -> int:
-    for i, n in enumerate(update):
-        if n not in order:
+def is_ordered(update: list[int], ordering_rules: dict[int, list]) -> bool:
+    for i, num in enumerate(update):
+        # If there are no ordering rules for this number skip!
+        if num not in ordering_rules:
             continue
+
+        # Otherwise iterate over every number before in sequence
+        # Return false if a previous page in sequence must be before current
         for j in range(i):
-            not_before = update[j]
-            if not_before in order[n]:
-                return i
-    return -1
+            previous_num = update[j]
+            if previous_num in ordering_rules[num]:
+                return False
+
+    return True
+
+
+def order(update: list[int], ordering_rules: dict[int, list]) -> None:
+    ordered = False
+
+    # If not ordered have another attempt
+    while not ordered:
+        for i in range(len(update)):
+            num = update[i]
+
+            # If there are no ordering rules for this number skip!
+            if num not in ordering_rules:
+                continue
+
+            # Otherwise iterate over every number before in sequence
+            # If found number that current number is supposed to be before
+            #   and insert number before it
+            for j in range(i):
+                previous_num = update[j]
+                if previous_num in ordering_rules[num]:
+                    update.pop(i)
+                    update.insert(j, num)
+                    break
+
+        ordered = is_ordered(update, ordering_rules)
 
 
 # Try to read a path to an input file from command line arguments
 path = sys.argv[1] if len(sys.argv) > 1 else "input.in"
 
-order: dict[int, list[int]] = {}
+# Parse input file into a dictionary for rules and a 2D array for page updates
+ordering_rules = defaultdict(list)
 updates = []
 with open(path, "r") as f:
-    line = f.readline().strip()
+    rules, page_updates = f.read().split("\n\n")
 
-    while line:
-        x, y = map(int, line.split('|'))
-        if x in order:
-            order[x].append(y)
-        else:
-            order[x] = [y]
-        line = f.readline().strip()
+    for rule in rules.split('\n'):
+        x, y = map(int, rule.split('|'))
+        ordering_rules[x].append(y)
 
-    line = f.readline().strip()
-    while line:
-        updates.append(list(map(int, line.strip().split(','))))
-        line = f.readline().strip()
+    for page_update in page_updates.strip().split('\n'):
+        updates.append(list(map(int, page_update.split(','))))
 
+# If update is not sorted then sort and sum middle term
 middle_total = 0
-for i, update in enumerate(updates):
-    return_code = valid(update)
-
-    if return_code == -1:
+for update in updates:
+    # Skip if already sorted
+    if is_ordered(update, ordering_rules):
         continue
 
-    while return_code != -1:
-        # Re-order
-        value = update.pop(return_code)
+    # Order pages in update
+    order(update, ordering_rules)
 
-        last_return_code = return_code
-        for j in range(len(update)+1):
-            update.insert(j, value)
-
-            if valid(update) != last_return_code:
-                break
-
-            value = update.pop(j)
-
-        return_code = valid(update)
-
-    middle = len(update) // 2
-    middle_total += update[middle]
-
+    middle_index = len(update) // 2
+    middle_total += update[middle_index]
 
 print(f"Solution:\n{middle_total}")
