595820720e765b0d7b239e874e67c7f8a1983246
Seb Zanardo <sebzanardo@optusnet.com.au>
2024-12-12 10:47:34 +1100

    Optimised solutions for Day 10

--- solutions/10- Hoof It/part1.py
+++ solutions/10- Hoof It/part1.py
@@ -1,5 +1,4 @@
 import sys
-from queue import deque
 
 
 DIRECTIONS = ((1, 0), (0, 1), (-1, 0), (0, -1))
@@ -8,6 +7,7 @@ DIRECTIONS = ((1, 0), (0, 1), (-1, 0), (0, -1))
 # Try to read a path to an input file from command line arguments
 path = sys.argv[1] if len(sys.argv) > 1 else "input.in"
 
+# Parse input file into 2D integer array and an array of starting positions
 start_positions = []
 map_heights = []
 with open(path, "r") as f:
@@ -22,13 +22,14 @@ with open(path, "r") as f:
                 row.append(int(height))
         map_heights.append(row)
 
+# Find width and height of map bounds
 width = len(map_heights[0])
 height = len(map_heights)
 
+# Traverse all trails and count total branches that have a unique end position
 total_branches = 0
 for start in start_positions:
-    branches = 0
-    open = deque()
+    open = []
     open.append(start)
     seen_ends = set()
 
@@ -40,23 +41,26 @@ for start in start_positions:
             new_x = x + dir[0]
             new_y = y + dir[1]
 
+            # If we are outside of the map bounds, skip
             if new_x < 0 or new_x >= width or new_y < 0 or new_y >= height:
                 continue
 
             current_height = map_heights[y][x]
             new_height = map_heights[new_y][new_x]
 
+            # If we are not stepping up by one in height, skip
             if (new_height - current_height) != 1:
                 continue
 
+            # If are at the final height, check if already seen and if not add
             if new_height == 9:
                 if (new_x, new_y) not in seen_ends:
-                    branches += 1
                     seen_ends.add((new_x, new_y))
                 continue
 
             open.append((new_x, new_y))
 
-    total_branches += branches
+    # Add seen ends to total_branches
+    total_branches += len(seen_ends)
 
 print(f"Solution:\n{total_branches}")
--- solutions/10- Hoof It/part2.py
+++ solutions/10- Hoof It/part2.py
@@ -8,6 +8,7 @@ DIRECTIONS = ((1, 0), (0, 1), (-1, 0), (0, -1))
 # Try to read a path to an input file from command line arguments
 path = sys.argv[1] if len(sys.argv) > 1 else "input.in"
 
+# Parse input file into 2D integer array and an array of starting positions
 start_positions = []
 map_heights = []
 with open(path, "r") as f:
@@ -22,9 +23,11 @@ with open(path, "r") as f:
                 row.append(int(height))
         map_heights.append(row)
 
+# Find width and height of map bounds
 width = len(map_heights[0])
 height = len(map_heights)
 
+# Traverse all trails and count total branches
 total_branches = 0
 for start in start_positions:
     branches = 0
@@ -39,21 +42,25 @@ for start in start_positions:
             new_x = x + dir[0]
             new_y = y + dir[1]
 
+            # If we are outside of the map bounds, skip
             if new_x < 0 or new_x >= width or new_y < 0 or new_y >= height:
                 continue
 
             current_height = map_heights[y][x]
             new_height = map_heights[new_y][new_x]
 
+            # If we are not stepping up by one in height, skip
             if (new_height - current_height) != 1:
                 continue
 
+            # If are at the final height add to branches counter
             if new_height == 9:
                 branches += 1
                 continue
 
             open.append((new_x, new_y))
 
+    # Add branches from this starting position to total_branches
     total_branches += branches
 
 print(f"Solution:\n{total_branches}")
