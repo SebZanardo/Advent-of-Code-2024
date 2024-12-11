386fa5439e20a2758f8b335ab7ca8728574e87b1
Seb Zanardo <sebzanardo@optusnet.com.au>
2024-12-12 10:36:42 +1100

    Optimised solutions for Day 11

--- solutions/11- Plutonian Pebbles/part1.py
+++ solutions/11- Plutonian Pebbles/part1.py
@@ -1,35 +1,42 @@
 import sys
+from collections import Counter
 
 
+# Number of blinks to perform
 BLINKS = 25
 
 
 # Try to read a path to an input file from command line arguments
 path = sys.argv[1] if len(sys.argv) > 1 else "input.in"
 
-stones = []
+# Parse input file into hashmap
+stones_map = Counter()
 with open(path, "r") as f:
-    stones = list(map(int, f.readline().strip().split()))
+    for stone in f.readline().strip().split():
+        stones_map[int(stone)] += 1
 
-for _ in range(BLINKS):
-    for i in range(len(stones) - 1, -1, -1):
-        stone = stones[i]
+# Simulate blinks, simulating same sized stones in one step
+for i in range(BLINKS):
+    new_stone_map = Counter()
 
+    for stone, count in stones_map.items():
         # Zero
         if stone == 0:
-            stones[i] = 1
-            continue
+            new_stone_map[1] += count
 
         # Even digits
-        if len(str(stone)) % 2 == 0:
+        elif len(str(stone)) % 2 == 0:
             middle = len(str(stone)) // 2
             left = str(stone)[:middle]
             right = str(stone)[middle:]
-            stones[i] = int(left)
-            stones.insert(i + 1, int(right))
+            new_stone_map[int(left)] += count
+            new_stone_map[int(right)] += count
             continue
 
-        # None
-        stones[i] = stone * 2024
+        # None of the above
+        else:
+            new_stone_map[stone * 2024] += count
 
-print(f"Solution:\n{len(stones)}")
+    stones_map = new_stone_map
+
+print(f"Solution:\n{sum(stones_map.values())}")
--- solutions/11- Plutonian Pebbles/part2.py
+++ solutions/11- Plutonian Pebbles/part2.py
@@ -2,30 +2,30 @@ import sys
 from collections import Counter
 
 
+# Number of blinks to perform
 BLINKS = 75
 
 
 # Try to read a path to an input file from command line arguments
 path = sys.argv[1] if len(sys.argv) > 1 else "input.in"
 
-stones = []
+# Parse input file into hashmap
+stones_map = Counter()
 with open(path, "r") as f:
-    stones = list(map(int, f.readline().strip().split()))
-    stones_map = Counter()
-    for stone in stones:
-        stones_map[stone] += 1
+    for stone in f.readline().strip().split():
+        stones_map[int(stone)] += 1
 
-for _ in range(BLINKS):
+# Simulate blinks, simulating same sized stones in one step
+for i in range(BLINKS):
     new_stone_map = Counter()
 
     for stone, count in stones_map.items():
         # Zero
         if stone == 0:
             new_stone_map[1] += count
-            continue
 
         # Even digits
-        if len(str(stone)) % 2 == 0:
+        elif len(str(stone)) % 2 == 0:
             middle = len(str(stone)) // 2
             left = str(stone)[:middle]
             right = str(stone)[middle:]
@@ -33,8 +33,9 @@ for _ in range(BLINKS):
             new_stone_map[int(right)] += count
             continue
 
-        # None
-        new_stone_map[stone * 2024] += count
+        # None of the above
+        else:
+            new_stone_map[stone * 2024] += count
 
     stones_map = new_stone_map
 
